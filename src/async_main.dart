f918b627923b41f6a76dce11f04cc81d620e45f0
phuquy2114 <phuquycntt@gmail.com>
2023-02-12 20:27:23 +0700

    init code

--- src/async_main.dart
+++ src/async_main.dart
@@ -7,21 +7,36 @@ Future<String> fetchUserOrder() =>
     // Imagine that this function is more complex and slow.
     Future.delayed(
       const Duration(seconds: 2),
-      () => 'Large Latte',
+      () => 'Large Latte 2',
     );
 
+Future<String> fetchUserOrder5Second() =>
+    // Imagine that this function is more complex and slow.
+    Future.delayed(
+      const Duration(seconds: 5),
+      () => 'Large Latte 5 ',
+    );
+
+Future<void> normalFunc() async {
+  // Imagine that this function is more complex and slow.
+  for (int i = 0; i < 1000; i++) {
+    print(i);
+  }
+}
 // handle Error
 
 Future handleError() async {
   try {
-    var order = await fetchUserOrder();
-    print('Awaiting user order...$order');
+    var order = await fetchUserOrder(); // 2second
+    var order2 = fetchUserOrder5Second(); // 5second
+    normalFunc();
+    print('A waiting user order...$order $order2');
   } catch (err) {
     print('Caught error: $err');
   }
 }
 
 void main() {
-  print(createOrderMessage());
+  // print(createOrderMessage());
   handleError();
 }
--- src/synchronus_main.dart
+++ src/synchronus_main.dart
@@ -5,7 +5,16 @@ Future write(List<int> values) async {
   }
 }
 
+Future read(List<int> values) async {
+  for (int value in values) {
+    await Future.delayed(new Duration(seconds: 5));
+    print(value);
+  }
+}
+
+
 void main() {
   List<int> list = [1, 2, 3, 4];
   write(list);
+  read(list);
 }
